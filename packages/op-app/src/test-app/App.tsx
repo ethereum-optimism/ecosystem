4fd1fd7b7bb1b373b1939c7ff75309a88a11a23a
italiano@oplabs.co <italiano@oplabs.co>
2023-11-30 19:54:31 -0500

    remove op-wagmi test

--- packages/op-app/src/test-app/App.tsx
+++ packages/op-app/src/test-app/App.tsx
@@ -1,38 +1,11 @@
-import { useSimulateDepositETH } from 'op-wagmi'
 import { useCallback } from 'react'
-import { parseEther } from 'viem'
 import type { Connector} from 'wagmi';
 import { useAccount, useConnect } from 'wagmi'
 import { walletConnect } from 'wagmi/connectors'
 
 import { useIsNetworkUnsupported } from '../hooks/useIsNetworkUnsupported'
 import { useNetworkPair } from '../hooks/useNetworkPair'
-import { useOPWagmiConfig } from '../hooks/useOPWagmiConfig'
 import { OPAppProvider } from '../providers/OPAppProvider'
-
-const SimulateButton = () => {
-  const { address } = useAccount()
-  const { currentNetworkPair } = useNetworkPair()
-  const { opConfig } = useOPWagmiConfig()
-
-  const { status: simulateStatus, refetch: simulateDepositETH } = useSimulateDepositETH({
-    args: {
-      to: address as `0x{string}`,
-      amount: parseEther('0.01'),
-    },
-    l2ChainId: currentNetworkPair?.l2.id ?? 0,
-    query: { enabled: false, retry: false },
-    // @ts-ignore type isn't expored currently
-    config: opConfig,
-  })
-
-  return (
-    <button onClick={() => {
-      simulateDepositETH()
-      console.log(simulateStatus)
-    }}>Simulate</button>
-  )
-}
 
 const connectors = [
   walletConnect({ projectId: 'e9c485da698064d6df5c19c5d12a845c' })
@@ -66,7 +39,6 @@ const Demo = () => {
           <div>L1: {currentNetworkPair?.l1.name}</div>
           <div>L2: {currentNetworkPair?.l2.name}</div>
         </h2>
-        {chain?.id && <SimulateButton />}
       </center>
     </div>
   )
