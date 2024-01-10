1fd3cfca5a12cbb1c824e1670233b0779b756235
Imran Jami <imranajami@gmail.com>
2024-01-10 15:53:42 -0800

    Creating a playground environment for devs

---
+++ packages/ui-components/playground/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { TestComponent } from '@eth-optimism/ui-components'
+
+function App() {
+  return (
+    <div className="bg-red-500">
+      <TestComponent />
+    </div>
+  )
+}
+
+export default App
---
+++ packages/ui-components/playground/tailwind.config.js
@@ -0,0 +1,7 @@
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
