505b4f103da9b889dd651ef03ab8ad01f3ea522e
Imran Jami <imranajami@gmail.com>
2024-01-09 19:27:09 -0800

    add linter

--- packages/ui-components/vite.config.ts
+++ packages/ui-components/vite.config.ts
@@ -1,7 +1,7 @@
 // vite.config.js
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
