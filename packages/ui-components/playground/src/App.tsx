d2fb6f193ec57512febb6fedcb65ffe5a96b97c3
Imran Jami <imranajami@gmail.com>
2024-01-12 12:31:08 -0800

    adding accordion

--- packages/ui-components/playground/src/App.tsx
+++ packages/ui-components/playground/src/App.tsx
@@ -1,11 +1,38 @@
 import './App.css'
 
-import { Button } from '@eth-optimism/ui-components'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@eth-optimism/ui-components'
 
 function App() {
   return (
     <div>
-      <Button>TEST</Button>
+      <Accordion type="single" collapsible className="w-full">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Is it styled?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It comes with default styles that matches the other
+            components&apos; aesthetic.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>Is it animated?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It&apos;s animated by default, but you can disable it if you
+            prefer.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+      <p>test test test</p>
     </div>
   )
 }
