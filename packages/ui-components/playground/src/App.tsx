8b25308c6ae420f37af97762ec7fc80a89be7cc4
jvmi <imranjami@users.noreply.github.com>
2024-01-17 13:43:39 -0800

    Fixed TS errors for drawer,  form, & command (#42)
    * Fixed ts errors for drawer + form
    
    * + command
    
    * fix linter

--- packages/ui-components/playground/src/App.tsx
+++ packages/ui-components/playground/src/App.tsx
@@ -1,40 +1,35 @@
 import './App.css'
 
 import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
 } from '@eth-optimism/ui-components'
 
 function App() {
   return (
     <div>
-      <ResizablePanelGroup
-        direction="horizontal"
-        className="max-w-md rounded-lg border"
-      >
-        <ResizablePanel defaultSize={50}>
-          <div className="flex h-[200px] items-center justify-center p-6">
-            <span className="font-semibold">One</span>
-          </div>
-        </ResizablePanel>
-        <ResizableHandle />
-        <ResizablePanel defaultSize={50}>
-          <ResizablePanelGroup direction="vertical">
-            <ResizablePanel defaultSize={25}>
-              <div className="flex h-full items-center justify-center p-6">
-                <span className="font-semibold">Two</span>
-              </div>
-            </ResizablePanel>
-            <ResizableHandle />
-            <ResizablePanel defaultSize={75}>
-              <div className="flex h-full items-center justify-center p-6">
-                <span className="font-semibold">Three</span>
-              </div>
-            </ResizablePanel>
-          </ResizablePanelGroup>
-        </ResizablePanel>
-      </ResizablePanelGroup>
+      <Command>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>Calendar</CommandItem>
+            <CommandItem>Search Emoji</CommandItem>
+            <CommandItem>Calculator</CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>Profile</CommandItem>
+            <CommandItem>Billing</CommandItem>
+            <CommandItem>Settings</CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </Command>
     </div>
   )
 }
