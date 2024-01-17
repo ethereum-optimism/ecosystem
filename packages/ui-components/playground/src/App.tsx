c73fd7a9bb78fe7a53e149b6c4162de48501c63a
jvmi <imranjami@users.noreply.github.com>
2024-01-16 16:50:21 -0800

    Add in all the shadcn/ui components (#35)
    * Add in all the shadcn/ui components
    
    * adding accordion
    
    * update
    
    * fix imports
    
    * Delete package-lock.json
    
    * Update pnpm-lock.yaml
    
    * linter

--- packages/ui-components/playground/src/App.tsx
+++ packages/ui-components/playground/src/App.tsx
@@ -1,11 +1,27 @@
 import './App.css'
 
-import { Button } from '@eth-optimism/ui-components'
+import {
+  Button,
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@eth-optimism/ui-components'
 
 function App() {
   return (
     <div>
-      <Button>TEST</Button>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button variant="outline">Hover</Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Add to library</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <p>test test test</p>
     </div>
   )
 }
--- packages/ui-components/src/TestComponent.tsx
+++
@@ -1,9 +0,0 @@
-const TestComponent = () => {
-  return (
-    <div>
-      <p className="bg-red-800">TESTTTT</p>
-    </div>
-  )
-}
-
-export { TestComponent }
