4cd1389722acfaa5d696083b85bf263b7549b83e
Nick Italiano <italiano@oplabs.co>
2024-01-17 17:31:35 -0500

    Drops Goerli Support (#43)
    drop goerli support

--- packages/op-app/src/configs/networkPairs.ts
+++ packages/op-app/src/configs/networkPairs.ts
@@ -2,12 +2,9 @@
 import type { Chain } from 'viem/chains'
 import {
   base,
-  baseGoerli,
   baseSepolia,
-  goerli,
   mainnet,
   optimism,
-  optimismGoerli,
   optimismSepolia,
   pgn,
   pgnTestnet,
@@ -21,29 +18,27 @@ import { lyra, mode, orderlyNetwork } from './chains'
 
 export type NetworkType =
   | 'base'
-  | 'op'
   | 'lyra'
   | 'mode'
+  | 'op'
   | 'orderly'
   | 'pgn'
   | 'zora'
 
 export const networkPairsByGroup: NetworkPairGroup = {
   base: {
-    goerli: [goerli, baseGoerli],
     mainnet: [mainnet, base],
     sepolia: [sepolia, baseSepolia],
-  },
-  op: {
-    goerli: [goerli, optimismGoerli],
-    mainnet: [mainnet, optimism],
-    sepolia: [sepolia, optimismSepolia],
   },
   lyra: {
     mainnet: [mainnet, lyra],
   },
   mode: {
     mainnet: [mainnet, mode],
+  },
+  op: {
+    mainnet: [mainnet, optimism],
+    sepolia: [sepolia, optimismSepolia],
   },
   orderly: {
     mainnet: [mainnet, orderlyNetwork],
@@ -59,8 +54,6 @@ export const networkPairsByGroup: NetworkPairGroup = {
 }
 
 export const networkPairsByID: Record<number, [Chain, Chain]> = {
-  84531: [goerli, baseGoerli],
-  420: [goerli, optimismGoerli],
   8453: [mainnet, base],
   957: [mainnet, lyra],
   34443: [mainnet, mode],
