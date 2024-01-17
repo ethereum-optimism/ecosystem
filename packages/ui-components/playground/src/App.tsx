cf4028460d167e82d820368a249722721dd1c855
jvmi <imranjami@users.noreply.github.com>
2024-01-17 08:59:06 -0800

    Fix ts errors in resizable component (#41)
    * Fix ts errors in resizable component
    
    * Update resizable.tsx

--- packages/ui-components/playground/src/App.tsx
+++ packages/ui-components/playground/src/App.tsx
@@ -1,27 +1,40 @@
 import './App.css'
 
 import {
-  Button,
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
 } from '@eth-optimism/ui-components'
 
 function App() {
   return (
     <div>
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button variant="outline">Hover</Button>
-          </TooltipTrigger>
-          <TooltipContent>
-            <p>Add to library</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
-      <p>test test test</p>
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="max-w-md rounded-lg border"
+      >
+        <ResizablePanel defaultSize={50}>
+          <div className="flex h-[200px] items-center justify-center p-6">
+            <span className="font-semibold">One</span>
+          </div>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={50}>
+          <ResizablePanelGroup direction="vertical">
+            <ResizablePanel defaultSize={25}>
+              <div className="flex h-full items-center justify-center p-6">
+                <span className="font-semibold">Two</span>
+              </div>
+            </ResizablePanel>
+            <ResizableHandle />
+            <ResizablePanel defaultSize={75}>
+              <div className="flex h-full items-center justify-center p-6">
+                <span className="font-semibold">Three</span>
+              </div>
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   )
 }
