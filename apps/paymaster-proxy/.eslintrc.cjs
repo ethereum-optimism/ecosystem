4fb10eda22c812f01982217d31642540425cca15
James Kim <jakim929@gmail.com>
2024-01-25 16:19:52 -0500

    revert to using monorepo's base eslint configs

--- apps/paymaster-proxy/.eslintrc.cjs
+++ apps/paymaster-proxy/.eslintrc.cjs
@@ -1,8 +1,3 @@
-/* eslint-env node */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  root: true,
-  env: { es2020: true },
+  extends: ['../../.eslintrc.js'],
 }
