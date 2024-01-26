7ad4bfa1e56cece368aa226c0d5b539bd5dc8d9e
Imran Jami <imranajami@gmail.com>
2024-01-25 18:10:09 -0800

    linter

--- apps/dapp-console/app/page.tsx
+++ apps/dapp-console/app/page.tsx
@@ -1,3 +1,12 @@
+'use client'
+
+import { Button } from '@eth-optimism/ui-components'
+
 export default function Home() {
-  return <main>welcome to the dapp console</main>
+  return (
+    <main>
+      welcome to the dapp console
+      <Button>Test Button</Button>
+    </main>
+  )
 }
