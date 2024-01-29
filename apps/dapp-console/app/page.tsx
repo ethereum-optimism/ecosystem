87cc5975cb6a8a968c0cbc8f700a97fcc6de3f83
jvmi <imranjami@users.noreply.github.com>
2024-01-29 11:06:50 -0800

    Create new package in apps directory for dapp console (#53)
    * Create new package in apps directory for dapp console
    
    * linter
    
    * fixes

---
+++ apps/dapp-console/app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { Button } from '@eth-optimism/ui-components'
+
+export default function Home() {
+  return (
+    <main>
+      welcome to the dapp console
+      <Button>Test Button</Button>
+    </main>
+  )
+}
