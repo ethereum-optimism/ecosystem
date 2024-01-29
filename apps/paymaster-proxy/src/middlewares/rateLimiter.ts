e651737efb02b00c6eabf8fd509f1fc31f07f889
James Kim <jakim929@gmail.com>
2024-01-29 17:53:29 -0500

    fix comments

--- apps/paymaster-proxy/src/middlewares/rateLimiter.ts
+++ apps/paymaster-proxy/src/middlewares/rateLimiter.ts
@@ -2,7 +2,7 @@ import { rateLimit } from 'express-rate-limit'
 
 export const rateLimiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-  standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  limit: 100,
+  standardHeaders: 'draft-7',
+  legacyHeaders: false,
 })
