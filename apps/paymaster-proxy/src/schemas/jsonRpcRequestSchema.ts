4fbe2cc974d892baabe9b271bd7dc40879573e89
James Kim <jakim929@gmail.com>
2024-01-31 10:24:50 -0500

    add jsonRpcRequestSchema

---
+++ apps/paymaster-proxy/src/schemas/jsonRpcRequestSchema.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod'
+
+export const jsonRpcRequestSchema = z
+  .object({
+    jsonrpc: z.literal('2.0'),
+    id: z.number(),
+    method: z.string(),
+    params: z.unknown(),
+  })
+  .strict()
