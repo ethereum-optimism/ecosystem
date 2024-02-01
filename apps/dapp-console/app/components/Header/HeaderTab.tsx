7f281e805e5cad8cc5f840690e8c1d6076e7b665
Imran Jami <imranajami@gmail.com>
2024-01-31 16:26:45 -0800

    Implement Support dropdown

--- apps/dapp-console/app/components/Header/HeaderTab.tsx
+++ apps/dapp-console/app/components/Header/HeaderTab.tsx
@@ -2,6 +2,18 @@ import { Route, routes } from '@/app/constants'
 import { cn } from '@/app/lib/utils'
 import Link from 'next/link'
 import { Text } from '@eth-optimism/ui-components/src/components/ui/text'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+} from '@eth-optimism/ui-components/src/components/ui/dropdown-menu'
+
+import { Button } from '@eth-optimism/ui-components/src/components/ui/button'
+import { RiArrowDownSLine } from '@remixicon/react'
+import { useState } from 'react'
 
 type HeaderTabsProps = {
   currentRoute: Route['path']
@@ -10,34 +22,39 @@ type HeaderTabsProps = {
 const HeaderTabs = ({ currentRoute }: HeaderTabsProps) => {
   return (
     <div className="flex gap-8 items-end h-full">
-      {Object.keys(routes).map((route) => {
-        const { path, label } = routes[route]
-        return (
-          <HeaderTabItem
-            key={path}
-            href={path}
-            isActive={currentRoute === path}
-          >
-            <Text as="span">{label}</Text>
-          </HeaderTabItem>
-        )
-      })}
+      <HeaderTabItem
+        href={routes.CONSOLE.path}
+        isActive={currentRoute === routes.CONSOLE.path}
+      >
+        <Text as="span">{routes.CONSOLE.label}</Text>
+      </HeaderTabItem>
+      <HeaderTabItem
+        href={routes.INSIGHTS.path}
+        isActive={currentRoute === routes.INSIGHTS.path}
+      >
+        <Text as="span">{routes.INSIGHTS.label}</Text>
+      </HeaderTabItem>
+      <SupportDropdownMenu />
     </div>
   )
 }
 
+const baseClasses =
+  'pb-6 text-muted-foreground font-medium border-b-4 border-transparent transition-all duration-200 ease-in-out'
+const hoverClasses = 'hover:border-muted-foreground hover:text-foreground'
+const activeClasses = 'border-foreground text-foreground'
+
 type HeaderTabItemProps = {
-  href: string
+  href?: string
   isActive?: boolean
   children: React.ReactNode
 }
 
-const HeaderTabItem = ({ href, isActive, children }: HeaderTabItemProps) => {
-  const baseClasses =
-    'pb-6 text-muted-foreground font-medium border-b-4 border-transparent transition-all duration-200 ease-in-out'
-  const hoverClasses = 'hover:border-muted-foreground hover:text-foreground'
-  const activeClasses = 'border-foreground text-foreground'
-
+const HeaderTabItem = ({
+  href = '',
+  isActive,
+  children,
+}: HeaderTabItemProps) => {
   return (
     <Link
       href={href}
@@ -45,6 +62,35 @@ const HeaderTabItem = ({ href, isActive, children }: HeaderTabItemProps) => {
     >
       {children}
     </Link>
+  )
+}
+
+const SupportDropdownMenu = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+
+  return (
+    <DropdownMenu
+      onOpenChange={(isOpen) => {
+        setIsDropdownOpen(isOpen)
+      }}
+    >
+      <DropdownMenuTrigger
+        className={cn('flex items-center gap-2', baseClasses, hoverClasses)}
+      >
+        <Text as="span">Support</Text>
+        <RiArrowDownSLine
+          className={cn('h-4 w-4 transition-transform duration-200', {
+            'rotate-180': isDropdownOpen,
+          })}
+        />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-80">
+        <DropdownMenuLabel>
+          <Text as="span">Support</Text>
+        </DropdownMenuLabel>
+        <DropdownMenuItem>heeeello</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
