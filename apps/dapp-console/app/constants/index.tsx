6253ebb5477ce08b631245dff20c0d240b9717fb
Imran Jami <imranajami@gmail.com>
2024-02-01 10:35:15 -0800

    Implement support dropdown menu

--- apps/dapp-console/app/constants/index.tsx
+++ apps/dapp-console/app/constants/index.tsx
@@ -16,4 +16,57 @@ const routes: Routes = {
   },
 }
 
-export { routes }
+const externalRoutes: Routes = {
+  // Support items
+  DEV_FORUM: {
+    path: '',
+    label: 'Developer Forum',
+  },
+  FARCASTER: {
+    path: '',
+    label: 'Farcaster',
+  },
+  DISCORD: {
+    path: '',
+    label: 'Discord',
+  },
+  DAPP_EXAPMPLES: {
+    path: '',
+    label: 'Dapp Examples',
+  },
+  // Documentation items
+  ETH_DOCS: {
+    path: '',
+    label: 'Ethereum',
+  },
+  BASE_DOCS: {
+    path: '',
+    label: 'Base',
+  },
+  FRAX_DOCS: {
+    path: '',
+    label: 'Fraxtal',
+  },
+  LISK_DOCS: {
+    path: '',
+    label: 'Lisk',
+  },
+  MODE_DOCS: {
+    path: '',
+    label: 'Mode',
+  },
+  OPTIMISM_DOCS: {
+    path: '',
+    label: 'Optimism',
+  },
+  REDSTONE_DOCS: {
+    path: '',
+    label: 'Redstone',
+  },
+  ZORA_DOCS: {
+    path: '',
+    label: 'Zora',
+  },
+}
+
+export { routes, externalRoutes }
