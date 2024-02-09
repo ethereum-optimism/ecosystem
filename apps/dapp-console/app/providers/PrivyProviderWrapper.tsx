0391ce458c3d7c0939c4c5e2b368d524395473b9
Imran Jami <imranajami@gmail.com>
2024-02-08 17:36:06 -0800

    Update PrivyProviderWrapper.tsx

--- apps/dapp-console/app/providers/PrivyProviderWrapper.tsx
+++ apps/dapp-console/app/providers/PrivyProviderWrapper.tsx
@@ -1,18 +1,21 @@
 'use client'
 
 import { PrivyProvider } from '@privy-io/react-auth'
+import { useTheme } from 'next-themes'
 
 // This is a public app_id provided in the privy docs: https://docs.privy.io/guide/quickstart
 const PRIVY_PUBLIC_APP_ID = 'clpispdty00ycl80fpueukbhl'
 
 function PrivyProviderWrapper({ children }: { children: React.ReactNode }) {
+  const { theme } = useTheme()
+
   return (
     <PrivyProvider
       appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || PRIVY_PUBLIC_APP_ID}
       config={{
         loginMethods: ['email'],
         appearance: {
-          theme: 'light',
+          theme: theme === 'dark' ? 'dark' : 'light',
           accentColor: '#FF0420',
           logo: '/logos/superchain-developer-logo.png',
         },
