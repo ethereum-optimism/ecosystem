7ece81026078530582671f3eb4c6ca33f7513013
James Kim <jakim929@gmail.com>
2024-02-09 10:34:24 -0500

    feat(contracts): create new contracts-ecosystem package (#109)
    * chore: forge init
    
    * feat(contracts): create new ecosystem-contracts package
    
    * fix solhint config issue
    
    * add foundry toolchain to lint action

---
+++ packages/contracts-ecosystem/test/SimpleNFT.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.21;
+
+import {Test, console} from "forge-std/Test.sol";
+import {SimpleNFT} from "../src/SimpleNFT.sol";
+
+contract SimpleNFTTest is Test {
+    SimpleNFT public simpleNFT;
+    address bob;
+    address sally;
+
+    function setUp() public {
+        simpleNFT = new SimpleNFT();
+
+        bob = makeAddr("bob");
+        sally = makeAddr("sally");
+    }
+
+    function test_mint_mintToMsgSender_succeeds() public {
+        vm.prank(bob);
+        simpleNFT.mint();
+
+        assertEq(simpleNFT.balanceOf(bob), 1);
+    }
+
+    function test_mint_mintToSomeoneElse_succeeds() public {
+        vm.prank(bob);
+        simpleNFT.mintTo(sally);
+
+        assertEq(simpleNFT.balanceOf(bob), 0);
+        assertEq(simpleNFT.balanceOf(sally), 1);
+    }
+}
