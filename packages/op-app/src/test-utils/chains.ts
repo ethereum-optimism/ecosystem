aa7c1b09e05c76718dbc58c96616db01a7481cbc
Nick Italiano <italiano@oplabs.co>
2024-02-12 18:38:42 -0500

    Fixes incorrect vitest test pool env var (#117)
    fixes incorrect vitest test pool env var

--- packages/op-app/src/test-utils/chains.ts
+++ packages/op-app/src/test-utils/chains.ts
@@ -6,7 +6,7 @@ import { L1_PORT, L2_PORT } from './constants'
 
 const [mainnet, opMainnet] = networkPairsByGroup.op.mainnet
 
-const vitestPool = process.env.VITEST_vitestPool_ID ?? 1
+const vitestPool = process.env.VITEST_POOL_ID ?? 1
 
 export const l1ForkURL = mainnet.rpcUrls.default.http[0]
 export const l2ForkURL = optimism.rpcUrls.default.http[0]
