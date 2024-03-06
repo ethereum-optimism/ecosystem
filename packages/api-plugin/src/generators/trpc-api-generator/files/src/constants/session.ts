bbebdeeaf5ef4e41b048b44d5291ce674b1697c8
tre <tremarkley@gmail.com>
2024-03-06 10:37:38 -0800

    add iron session to trpc-api generator (#154)

---
+++ packages/api-plugin/src/generators/trpc-api-generator/files/src/constants/session.ts
@@ -0,0 +1,18 @@
+import { type IronSession, type SessionOptions } from 'iron-session'
+
+import { envVars } from './envVars'
+
+export const sessionOptions: SessionOptions = {
+  password: envVars.IRON_SESSION_SECRET,
+  cookieName: '<%= name %>.auth.session-token',
+  ttl: 60 * 60 * 24 * 14, // 14 days
+  cookieOptions: {
+    secure: true,
+    sameSite: 'none',
+    httpOnly: true,
+  },
+}
+
+export type SessionData = {}
+
+export type Session = IronSession<SessionData>
