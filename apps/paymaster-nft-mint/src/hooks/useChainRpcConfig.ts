fff192dfe19fe0914b60e968b2f868708692de8f
James Kim <jakim929@gmail.com>
2024-03-11 15:28:03 -0400

    fix(paymaster-example): refactor functions to make it closer to the tutorial (#170)

--- apps/paymaster-nft-mint/src/hooks/useChainRpcConfig.ts
+++ apps/paymaster-nft-mint/src/hooks/useChainRpcConfig.ts
@@ -4,15 +4,22 @@ import { useChainId } from 'wagmi'
 
 const aaRpcConfigByChainId: Record<
   number,
-  { chain: Chain; paymasterRpcUrl: string; bundlerRpcUrl: string }
+  {
+    chain: Chain
+    rpcUrl: string
+    paymasterRpcUrl: string
+    bundlerRpcUrl: string
+  }
 > = {
   [sepolia.id]: {
     chain: sepolia,
+    rpcUrl: envVars.VITE_RPC_URL_SEPOLIA,
     paymasterRpcUrl: envVars.VITE_PAYMASTER_RPC_URL_SEPOLIA,
     bundlerRpcUrl: envVars.VITE_BUNDLER_RPC_URL_SEPOLIA,
   },
   [optimismSepolia.id]: {
     chain: optimismSepolia,
+    rpcUrl: envVars.VITE_RPC_URL_OP_SEPOLIA,
     paymasterRpcUrl: envVars.VITE_PAYMASTER_RPC_URL_OP_SEPOLIA,
     bundlerRpcUrl: envVars.VITE_BUNDLER_RPC_URL_OP_SEPOLIA,
   },
--- apps/paymaster-nft-mint/src/libraries/aa-sdk/createModularAccountClientWithPaymaster.ts
+++ apps/paymaster-nft-mint/src/libraries/aa-sdk/createModularAccountClientWithPaymaster.ts
@@ -1,29 +1,61 @@
-import { SmartAccountSigner } from '@alchemy/aa-core'
+import {
+  SmartAccountSigner,
+  UserOperationRequest,
+  createSmartAccountClient,
+  deepHexlify,
+  resolveProperties,
+} from '@alchemy/aa-core'
 
-import { createMultiOwnerModularAccountClient } from '@alchemy/aa-accounts'
-import { sepolia } from 'viem/chains'
-import { superchainPaymasterMiddleware } from '@/libraries/aa-sdk/superchainPaymasterMiddleware'
+import { createMultiOwnerModularAccount } from '@alchemy/aa-accounts'
 import { Chain, http } from 'viem'
+import { createSuperchainPaymasterClient } from '@/libraries/aa-sdk/createSuperchainPaymasterClient'
 
-export const createModularAccountClientWithPaymaster = ({
+export const createModularAccountClientWithPaymaster = async ({
   signer,
+  rpcUrl,
   paymasterRpcUrl,
   bundlerRpcUrl,
   chain,
 }: {
   signer: SmartAccountSigner
+  rpcUrl: string
   paymasterRpcUrl: string
   bundlerRpcUrl: string
   chain: Chain
 }) => {
-  return createMultiOwnerModularAccountClient({
+  const superchainPaymasterClient = createSuperchainPaymasterClient({
+    paymasterRpcUrl,
+    chain,
+  })
+
+  const modularAccount = await createMultiOwnerModularAccount({
+    signer: signer,
+    chain: chain,
+    transport: http(rpcUrl),
+  })
+
+  return createSmartAccountClient({
     transport: http(bundlerRpcUrl),
     chain,
-    account: { signer },
-    ...superchainPaymasterMiddleware({
-      paymasterRpcUrl,
-      chain: sepolia,
-    }),
+    account: modularAccount,
+    gasEstimator: async (struct) => struct,
+    feeEstimator: async (struct) => struct,
+    paymasterAndData: {
+      paymasterAndData: async (struct, { account }) => {
+        const sponsorOperationResult =
+          await superchainPaymasterClient.sponsorUserOperation(
+            deepHexlify(
+              await resolveProperties(struct),
+            ) as UserOperationRequest,
+            account.getEntryPoint().address,
+          )
+        return {
+          ...struct,
+          ...sponsorOperationResult,
+        }
+      },
+      dummyPaymasterAndData: () => '0x',
+    },
   })
 }
 
