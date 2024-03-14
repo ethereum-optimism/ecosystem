1b119d4127f2c4105c66f3919d8d2fb2f39f7bc5
Nick Italiano <italiano@oplabs.co>
2024-03-14 14:17:05 -0400

    Adds Unlink Wallet Flow (#179)
    * adds unlink wallet flow
    
    * Adds shortenAddress to dapp-console
    
    * Adds aria label to unlink wallet button

---
+++ apps/dapp-console/app/settings/components/LinkedWallet.tsx
@@ -0,0 +1,73 @@
+import { shortenAddress } from '@eth-optimism/op-app'
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  Input,
+} from '@eth-optimism/ui-components'
+import { Text } from '@eth-optimism/ui-components/src/components/ui/text'
+import { WalletWithMetadata } from '@privy-io/react-auth'
+import { RiAlertFill, RiCloseLine } from '@remixicon/react'
+import { useCallback, useState } from 'react'
+
+export type LinkedWalletProps = {
+  wallet: WalletWithMetadata
+  onUnlink: (wallet: WalletWithMetadata) => void
+}
+
+export const LinkedWallet = ({ wallet, onUnlink }: LinkedWalletProps) => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
+
+  const handleUnlink = useCallback(() => onUnlink(wallet), [wallet, onUnlink])
+  const handleCancel = useCallback(
+    () => setIsDialogOpen(false),
+    [setIsDialogOpen],
+  )
+
+  return (
+    <div className="flex flex-row gap-2">
+      <Input className="rounded" value={wallet.address} disabled />
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogTrigger asChild>
+          <Button
+            className="rounded"
+            variant="secondary"
+            size="icon"
+            aria-label="Unlink Wallet"
+          >
+            <RiCloseLine />
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <div className="w-full flex flex-col items-center">
+            <RiAlertFill className="rounded" size={64} />
+            <Text
+              as="p"
+              className="cursor-default mt-4 mb-2 text-lg font-semibold"
+            >
+              Are you sure you want to unlink this wallet?
+            </Text>
+            <Text
+              as="p"
+              className="cursor-default mb-4 text-muted-foreground text-base"
+            >
+              {shortenAddress(wallet.address)}
+            </Text>
+
+            <Button className="rounded w-full mb-2" onClick={handleUnlink}>
+              Unlink
+            </Button>
+            <Button
+              className="rounded w-full"
+              variant="outline"
+              onClick={handleCancel}
+            >
+              Cancel
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </div>
+  )
+}
