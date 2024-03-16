7f6128ab29685b48802a991423daebd8e51356d3
James Kim <jakim929@gmail.com>
2024-03-16 13:19:24 -0400

    remove example app changes

--- apps/paymaster-nft-mint/src/config.ts
+++ apps/paymaster-nft-mint/src/config.ts
@@ -1,12 +1,11 @@
 import { envVars } from '@/envVars'
 import { http, createConfig } from 'wagmi'
-import { sepolia, optimismSepolia, baseSepolia } from 'wagmi/chains'
+import { sepolia, optimismSepolia } from 'wagmi/chains'
 
 export const config = createConfig({
-  chains: [optimismSepolia, baseSepolia, sepolia],
+  chains: [optimismSepolia, sepolia],
   transports: {
     [sepolia.id]: http(envVars.VITE_RPC_URL_SEPOLIA),
-    [baseSepolia.id]: http(envVars.VITE_RPC_URL_BASE_SEPOLIA),
     [optimismSepolia.id]: http(envVars.VITE_RPC_URL_OP_SEPOLIA),
   },
 })
