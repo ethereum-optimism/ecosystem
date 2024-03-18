c9eca82d8090af4f1dcdd35df776695a0416040c
James Kim <jakim929@gmail.com>
2024-03-18 16:46:38 -0400

    draft(storybook): create storybook app

---
+++ apps/ui-components-storybook/.storybook/main.js
@@ -0,0 +1,22 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+}
+export default config
