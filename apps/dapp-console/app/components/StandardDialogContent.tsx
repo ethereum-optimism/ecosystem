7ec3b3fbaf8ee5436762c3a16b307e8e19103250
Nick Italiano <italiano@oplabs.co>
2024-03-19 16:00:31 -0400

    Deployment Rebate Dialog (#191)
    * Adds StandardDialogContent to reuse dialog content styles
    
    * Adds DeploymentRebate model when setting ff is enabled
    
    * lint

---
+++ apps/dapp-console/app/components/StandardDialogContent.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image'
+
+import { Text } from '@eth-optimism/ui-components/src/components/ui/text'
+import { Badge } from '@eth-optimism/ui-components/src/components/ui/badge'
+
+export type DialogMetadata = {
+  label: string
+  title: string
+  description: React.ReactNode
+  primaryButton?: React.ReactNode
+  secondaryButton?: React.ReactNode
+  bannerImage?: string
+}
+
+export type StandardDialogContentProps = {
+  dialogMetadata: DialogMetadata
+}
+
+export const StandardDialogContent = ({
+  dialogMetadata,
+}: StandardDialogContentProps) => (
+  <div>
+    <Badge variant="secondary">
+      <Text as="p">{dialogMetadata.label}</Text>
+    </Badge>
+    {dialogMetadata.bannerImage && (
+      <Image
+        src={dialogMetadata.bannerImage}
+        alt={`${dialogMetadata.label} banner`}
+        className="w-full rounded-xs mt-6 object-cover"
+        width={450}
+        height={140}
+      />
+    )}
+    <div className="py-6">
+      <Text as="h3" className="text-lg font-semibold mb-2">
+        {dialogMetadata.title}
+      </Text>
+      <div className="text-md text-muted-foreground">
+        {dialogMetadata.description}
+      </div>
+    </div>
+    <div className="flex flex-col gap-2.5 w-full">
+      {dialogMetadata.primaryButton}
+      {dialogMetadata.secondaryButton}
+    </div>
+  </div>
+)
