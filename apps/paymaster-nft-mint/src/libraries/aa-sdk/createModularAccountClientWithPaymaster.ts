55e90906293327a0bc9a4123608ba07a00a2d985
James Kim <jakim929@gmail.com>
2024-03-20 12:22:49 -0400

    feat(paymaster-mint): update example with new chains (#198)
    * feat(paymaster-mint): update example with new chains
    
    * add support for other chains

--- apps/paymaster-nft-mint/src/libraries/aa-sdk/createModularAccountClientWithPaymaster.ts
+++ apps/paymaster-nft-mint/src/libraries/aa-sdk/createModularAccountClientWithPaymaster.ts
@@ -3,11 +3,12 @@ import {
   UserOperationRequest,
   createSmartAccountClient,
   deepHexlify,
+  getVersion060EntryPoint,
   resolveProperties,
 } from '@alchemy/aa-core'
 
 import { createMultiOwnerModularAccount } from '@alchemy/aa-accounts'
-import { Chain, http } from 'viem'
+import { Chain, custom, http } from 'viem'
 import { createSuperchainPaymasterClient } from '@/libraries/aa-sdk/createSuperchainPaymasterClient'
 
 export const createModularAccountClientWithPaymaster = async ({
@@ -32,10 +33,38 @@ export const createModularAccountClientWithPaymaster = async ({
     signer: signer,
     chain: chain,
     transport: http(rpcUrl),
+    factoryAddress: '0x000000e92D78D90000007F0082006FDA09BD5f11',
+    entryPoint: getVersion060EntryPoint(
+      chain,
+      '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789',
+    ),
   })
 
   return createSmartAccountClient({
-    transport: http(bundlerRpcUrl),
+    // Splits Bundler RPC and Public RPC traffic
+    // https://accountkit.alchemy.com/third-party/bundlers.html#splitting-bundler-traffic-and-node-rpc-traffic
+    transport: (opts) => {
+      const bundlerRpc = http(bundlerRpcUrl)(opts)
+      const publicRpc = http(rpcUrl)(opts)
+
+      return custom({
+        request: async (args) => {
+          const bundlerMethods = new Set([
+            'eth_sendUserOperation',
+            'eth_estimateUserOperationGas',
+            'eth_getUserOperationReceipt',
+            'eth_getUserOperationByHash',
+            'eth_supportedEntryPoints',
+          ])
+
+          if (bundlerMethods.has(args.method)) {
+            return bundlerRpc.request(args)
+          } else {
+            return publicRpc.request(args)
+          }
+        },
+      })(opts)
+    },
     chain,
     account: modularAccount,
     gasEstimator: async (struct) => struct,
--- apps/paymaster-nft-mint/src/pages/MainPage.tsx
+++ apps/paymaster-nft-mint/src/pages/MainPage.tsx
@@ -7,8 +7,16 @@ import {
   TabsList,
   TabsTrigger,
 } from '@eth-optimism/ui-components'
+import { baseSepolia, optimismSepolia, sepolia } from 'viem/chains'
+import { useChainId } from 'wagmi'
+
+const chainsSupportingKernel = [sepolia, optimismSepolia, baseSepolia]
 
 export const MainPage = () => {
+  const chainId = useChainId()
+  const isKernelSupportedByChain = chainsSupportingKernel.some(
+    ({ id }) => id === chainId,
+  )
   return (
     <div className="flex flex-col gap-4 p-8 items-center">
       <div className="text-3xl font-bold">
@@ -18,16 +26,24 @@ export const MainPage = () => {
         <ChainSwitcher />
       </div>
       <Tabs defaultValue="aa-sdk" className="w-[400px]">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="aa-sdk">Modular account</TabsTrigger>
-          <TabsTrigger value="permissionless">Kernel</TabsTrigger>
+        <TabsList className="w-full flex">
+          <TabsTrigger className="flex-1" value="aa-sdk">
+            Modular account
+          </TabsTrigger>
+          {isKernelSupportedByChain && (
+            <TabsTrigger className="flex-1" value="permissionless">
+              Kernel
+            </TabsTrigger>
+          )}
         </TabsList>
         <TabsContent value="aa-sdk" className="flex flex-col gap-4">
           <AaSdkExample />
         </TabsContent>
-        <TabsContent value="permissionless" className="flex flex-col gap-4">
-          <PermissionlessExample />
-        </TabsContent>
+        {isKernelSupportedByChain && (
+          <TabsContent value="permissionless" className="flex flex-col gap-4">
+            <PermissionlessExample />
+          </TabsContent>
+        )}
       </Tabs>
     </div>
   )
