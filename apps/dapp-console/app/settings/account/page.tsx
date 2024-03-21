4b98101c699c0673b7550ed1b521839e001624a1
Nick Italiano <italiano@oplabs.co>
2024-03-21 17:57:18 -0400

    Adds support for editing email and deleting user (#211)
    * Adds support for editing email and deleting user
    
    * remove rounded
    
    * bump privy version

--- apps/dapp-console/app/settings/account/page.tsx
+++ apps/dapp-console/app/settings/account/page.tsx
@@ -1,12 +1,32 @@
 'use client'
 
-import { Input } from '@eth-optimism/ui-components/src/components/ui/input/input'
-import { Label } from '@eth-optimism/ui-components/src/components/ui/label/label'
+import {
+  Button,
+  Input,
+  Label,
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  Separator,
+} from '@eth-optimism/ui-components'
+import { Text } from '@eth-optimism/ui-components/src/components/ui/text/text'
 
 import { usePrivy } from '@privy-io/react-auth'
+import { RiAlertFill, RiEdit2Fill } from '@remixicon/react'
+import { useCallback, useState } from 'react'
 
 export default function Account() {
-  const { user } = usePrivy()
+  const [isOpen, setOpen] = useState(false)
+  const { user, updateEmail } = usePrivy()
+
+  const handleDeleteAccount = useCallback(() => {
+    // TODO: add backend call here
+
+    // hard refresh after deleting user
+    window.location.href = '/'
+  }, [])
+
+  const handleCancel = useCallback(() => setOpen(false), [setOpen])
 
   if (!user) {
     return
@@ -17,12 +37,50 @@ export default function Account() {
       <Label htmlFor="email" className="mb-2">
         Email
       </Label>
-      <Input
-        type="email"
-        className="bg-secondary"
-        value={user.email?.address ?? ''}
-        disabled={true}
-      />
+
+      <div className="flex flex-row mb-10">
+        <Input
+          type="email"
+          className="bg-secondary cursor-default mr-2"
+          value={user.email?.address ?? ''}
+          disabled={true}
+        />
+        <Button variant="secondary" onClick={updateEmail}>
+          <Text as="span" className="hidden md:flex">
+            Edit
+          </Text>
+          <RiEdit2Fill className="flex md:hidden" />
+        </Button>
+      </div>
+
+      <Separator />
+
+      <Dialog open={isOpen} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button variant="ghost" className="mt-8 self-start">
+            <Text as="span" className="text-red-800">
+              Delete account
+            </Text>
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <div className="w-full flex flex-col items-center">
+            <RiAlertFill className="rounded" size={64} />
+          </div>
+          <Text
+            as="p"
+            className="cursor-default mt-4 mb-2 text-center text-lg font-semibold"
+          >
+            Are you sure you want to delete your account?
+          </Text>
+          <Button className="w-full" onClick={handleDeleteAccount}>
+            Delete account
+          </Button>
+          <Button className="w-full" variant="outline" onClick={handleCancel}>
+            Cancel
+          </Button>
+        </DialogContent>
+      </Dialog>
     </div>
   )
 }
