de958518b0eb3bb050ce005b2400b83889a4eae5
italiano@oplabs.co <italiano@oplabs.co>
2024-04-05 10:42:10 -0400

    adds example erc20

---
+++ packages/contracts-ecosystem/src/SimpleERC20.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {ERC20} from "solady/tokens/ERC20.sol";
+
+contract SimpleERC20 is ERC20 {
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+
+    constructor(string memory name_, string memory symbol_, uint8 decimals_) {
+        _name = name_;
+        _symbol = symbol_;
+        _decimals = decimals_;
+    }
+
+    function name() public view virtual override returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view virtual override returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
+}
