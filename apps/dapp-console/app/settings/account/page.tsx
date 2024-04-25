33199bc94ca358ea604c58b0008c657387642656
italiano@oplabs.co <italiano@oplabs.co>
2024-04-25 09:51:52 -0400

    Remove Delete Account UI

--- apps/dapp-console/app/settings/account/page.tsx
+++ apps/dapp-console/app/settings/account/page.tsx
@@ -4,29 +4,14 @@ import {
   Button,
   Input,
   Label,
-  Dialog,
-  DialogTrigger,
-  DialogContent,
-  Separator,
 } from '@eth-optimism/ui-components'
 import { Text } from '@eth-optimism/ui-components/src/components/ui/text/text'
 
 import { usePrivy } from '@privy-io/react-auth'
-import { RiAlertFill, RiEdit2Fill } from '@remixicon/react'
-import { useCallback, useState } from 'react'
+import { RiEdit2Fill } from '@remixicon/react'
 
 export default function Account() {
-  const [isOpen, setOpen] = useState(false)
   const { user, updateEmail } = usePrivy()
-
-  const handleDeleteAccount = useCallback(() => {
-    // TODO: add backend call here
-
-    // hard refresh after deleting user
-    window.location.href = '/'
-  }, [])
-
-  const handleCancel = useCallback(() => setOpen(false), [setOpen])
 
   if (!user) {
     return
@@ -43,7 +28,7 @@ export default function Account() {
           type="email"
           className="bg-secondary cursor-default mr-2"
           value={user.email?.address ?? ''}
-          disabled={true}
+          readOnly
         />
         <Button variant="secondary" onClick={updateEmail}>
           <Text as="span" className="hidden md:flex">
@@ -52,35 +37,6 @@ export default function Account() {
           <RiEdit2Fill className="flex md:hidden" />
         </Button>
       </div>
-
-      <Separator />
-
-      <Dialog open={isOpen} onOpenChange={setOpen}>
-        <DialogTrigger asChild>
-          <Button variant="ghost" className="mt-8 self-start">
-            <Text as="span" className="text-red-800">
-              Delete account
-            </Text>
-          </Button>
-        </DialogTrigger>
-        <DialogContent>
-          <div className="w-full flex flex-col items-center">
-            <RiAlertFill className="rounded" size={64} />
-          </div>
-          <Text
-            as="p"
-            className="cursor-default mt-4 mb-2 text-center text-lg font-semibold"
-          >
-            Are you sure you want to delete your account?
-          </Text>
-          <Button className="w-full" onClick={handleDeleteAccount}>
-            Delete account
-          </Button>
-          <Button className="w-full" variant="outline" onClick={handleCancel}>
-            Cancel
-          </Button>
-        </DialogContent>
-      </Dialog>
     </div>
   )
 }
