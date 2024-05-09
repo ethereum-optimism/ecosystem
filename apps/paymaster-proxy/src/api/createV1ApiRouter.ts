56cdaef25dbd31d5ca6290b2f03a84e56db01a41
James Kim <jakim929@gmail.com>
2024-05-09 13:58:53 -0400

    feat(paymaster-proxy): add mainnet support

--- apps/paymaster-proxy/src/api/createV1ApiRouter.ts
+++ apps/paymaster-proxy/src/api/createV1ApiRouter.ts
@@ -1,55 +1,28 @@
 import type { Router } from 'express'
 import express from 'express'
 import type { Logger } from 'pino'
-import { baseSepolia, optimismSepolia, sepolia, zoraSepolia } from 'viem/chains'
+import {
+  baseSepolia,
+  optimism,
+  optimismSepolia,
+  sepolia,
+  zoraSepolia,
+} from 'viem/chains'
 
 import type { ApiKeyServiceClient } from '@/apiKeyService/createApiKeyServiceClient'
-import type { TestnetChainConfig } from '@/config/ChainConfig'
+import type {
+  MainnetChainConfig,
+  TestnetChainConfig,
+} from '@/config/ChainConfig'
 import { chainConfigByChainId } from '@/config/chainConfigByChainId'
 import { fraxtalSepolia } from '@/constants/fraxtalSepolia'
 import type { Database } from '@/db/Database'
 import { createExpressRouterFromJsonRpcRouter } from '@/jsonRpc/createExpressRouterFromJsonRpcRouter'
 import type { Metrics } from '@/monitoring/metrics'
+import { createMainnetJsonRpcRouterWithAlchemyPaymasterProvider } from '@/paymasterProvider/alchemy/createMainnetJsonRpcRouterWithAlchemyPaymasterProvider'
 import { createTestnetJsonRpcRouterWithAlchemyPaymasterProvider } from '@/paymasterProvider/alchemy/createTestnetJsonRpcRouterWithAlchemyPaymasterProvider'
 
 export const V1_API_BASE_PATH = '/v1'
-
-const registerTestnetWithAlchemyPaymasterProvider = (
-  router: express.Router,
-  {
-    chainConfig,
-    database,
-    apiKeyServiceClient,
-    metrics,
-    logger,
-  }: {
-    chainConfig: TestnetChainConfig
-    database: Database
-    apiKeyServiceClient: ApiKeyServiceClient
-    metrics: Metrics
-    logger: Logger
-  },
-) => {
-  const { chain } = chainConfig
-  const defaultMetricLabels = { apiVersion: 'v1', chainId: chain.id }
-
-  const jsonRpcRouter = createTestnetJsonRpcRouterWithAlchemyPaymasterProvider({
-    chainConfig,
-    database,
-    apiKeyServiceClient,
-    metrics,
-    defaultMetricLabels,
-    logger,
-  })
-
-  const path = `/${chain.id}/rpc`
-
-  logger.info(
-    `Registering testnet paymaster RPC route at ${V1_API_BASE_PATH}${path}: ${chain.name}`,
-  )
-
-  router.use(path, createExpressRouterFromJsonRpcRouter(jsonRpcRouter))
-}
 
 export const createV1ApiRouter = ({
   metrics,
@@ -104,5 +77,86 @@ export const createV1ApiRouter = ({
     logger,
   })
 
+  registerMainnetWithAlchemyPaymasterProvider(router, {
+    chainConfig: chainConfigByChainId[optimism.id],
+    database,
+    apiKeyServiceClient,
+    metrics,
+    logger,
+  })
+
   return router
+}
+
+const registerTestnetWithAlchemyPaymasterProvider = (
+  router: express.Router,
+  {
+    chainConfig,
+    database,
+    apiKeyServiceClient,
+    metrics,
+    logger,
+  }: {
+    chainConfig: TestnetChainConfig
+    database: Database
+    apiKeyServiceClient: ApiKeyServiceClient
+    metrics: Metrics
+    logger: Logger
+  },
+) => {
+  const { chain } = chainConfig
+  const defaultMetricLabels = { apiVersion: 'v1', chainId: chain.id }
+
+  const jsonRpcRouter = createTestnetJsonRpcRouterWithAlchemyPaymasterProvider({
+    chainConfig,
+    database,
+    apiKeyServiceClient,
+    metrics,
+    defaultMetricLabels,
+    logger,
+  })
+
+  const path = `/${chain.id}/rpc`
+
+  logger.info(
+    `Registering testnet paymaster RPC route at ${V1_API_BASE_PATH}${path}: ${chain.name}`,
+  )
+
+  router.use(path, createExpressRouterFromJsonRpcRouter(jsonRpcRouter))
+}
+
+const registerMainnetWithAlchemyPaymasterProvider = (
+  router: express.Router,
+  {
+    chainConfig,
+    database,
+    apiKeyServiceClient,
+    metrics,
+    logger,
+  }: {
+    chainConfig: MainnetChainConfig
+    database: Database
+    apiKeyServiceClient: ApiKeyServiceClient
+    metrics: Metrics
+    logger: Logger
+  },
+) => {
+  const { chain } = chainConfig
+
+  const jsonRpcRouter = createMainnetJsonRpcRouterWithAlchemyPaymasterProvider({
+    chainConfig,
+    database,
+    apiKeyServiceClient,
+    metrics,
+    defaultMetricLabels: { apiVersion: 'v1', chainId: chain.id },
+    logger,
+  })
+
+  const path = `/${chain.id}/rpc`
+
+  logger.info(
+    `Registering mainnet paymaster RPC route at ${V1_API_BASE_PATH}${path}: ${chain.name}`,
+  )
+
+  router.use(path, createExpressRouterFromJsonRpcRouter(jsonRpcRouter))
 }
