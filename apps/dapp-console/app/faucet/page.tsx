c458e424f4d64e0772d7b16ed4af19e9f8fc0a61
Imran Jami <imranajami@gmail.com>
2024-05-15 17:55:35 -0400

    Implement card header

--- apps/dapp-console/app/faucet/page.tsx
+++ apps/dapp-console/app/faucet/page.tsx
@@ -1,12 +1,19 @@
 'use client'
 
+import { CardHeader } from '@eth-optimism/ui-components'
 import { Card } from '@eth-optimism/ui-components/src/components/ui/card/card'
 import { Text } from '@eth-optimism/ui-components/src/components/ui/text/text'
+import { FaucetHeader } from './components/FaucetHeader/FaucetHeader'
+import { useWallets } from '@privy-io/react-auth'
 
 export default function Faucet() {
+  const signedIn = false
+  const wallet = 'asdf'
+  const authentications = null
+
   return (
     <div className="flex flex-col w-full items-center py-10 px-6">
-      <div className="text-center">
+      <div className="text-center mb-10">
         <Text as="h1" className="text-5xl font-semibold mb-2">
           Superchain Faucet
         </Text>
@@ -15,7 +22,15 @@ export default function Faucet() {
         </Text>
       </div>
 
-      <Card>test</Card>
+      <Card>
+        <CardHeader>
+          <FaucetHeader
+            signedIn={signedIn}
+            wallet={wallet}
+            authentications={authentications}
+          />
+        </CardHeader>
+      </Card>
     </div>
   )
 }
