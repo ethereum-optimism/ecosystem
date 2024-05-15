597f3a5ee1d470130d1aa610a9f1317ac7a9f6ed
Imran Jami <imranajami@gmail.com>
2024-05-15 12:40:11 -0400

    add faucet page

--- apps/dapp-console/app/faucet/page.tsx
+++ apps/dapp-console/app/faucet/page.tsx
@@ -1,6 +1,11 @@
+'use client'
+
 import { Text } from '@eth-optimism/ui-components/src/components/ui/text/text'
+import { useFeatureFlag } from '../hooks/useFeatureFlag'
 
 export default function Faucet() {
+  const isSettingsEnabled = useFeatureFlag('enable_console_faucet')
+
   return (
     <div className="flex flex-col w-full items-center py-10 px-6">
       <div className="text-center">
@@ -8,7 +13,7 @@ export default function Faucet() {
           Superchain Faucet
         </Text>
         <Text as="p" className="text-lg text-center text-muted-foreground">
-          Get test tokens for building applications on the Superchain{' '}
+          Get test tokens for building applications on the Superchain
         </Text>
       </div>
     </div>
