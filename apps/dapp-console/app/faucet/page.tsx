c0149890317f274e6088dadf762c70deb63d9131
jvmi <10838132+jvmi7@users.noreply.github.com>
2024-05-23 16:05:29 -0700

    feat(faucet): Implement logo chain + fix styles (#356)
    * Implement logo chain + fix styles
    
    * Update page.tsx
    
    * fix linter and gate page
    
    * Update page.tsx
    
    * Update page.tsx
    
    * Update FaucetHeaderInner.tsx
    
    * Update FaucetHeaderInner.tsx

--- apps/dapp-console/app/faucet/page.tsx
+++ apps/dapp-console/app/faucet/page.tsx
@@ -4,19 +4,34 @@ import { CardHeader } from '@eth-optimism/ui-components'
 import { Card } from '@eth-optimism/ui-components/src/components/ui/card/card'
 import { Text } from '@eth-optimism/ui-components/src/components/ui/text/text'
 import { FaucetHeader } from '@/app/faucet/components/FaucetHeader/FaucetHeader'
+import { useEffect } from 'react'
+import { useFeatureFlag } from '@/app/hooks/useFeatureFlag'
+import { useRouter } from 'next/navigation'
 
 export default function Faucet() {
-  const signedIn = false
-  const wallet = false
+  const signedIn = true
+  const wallet = true
   const authentications = null
+  const isConsoleFaucetEnabled = useFeatureFlag('enable_console_faucet')
+  const router = useRouter()
+
+  // Redirect to the home page if the faucet is disabled
+  useEffect(() => {
+    if (!isConsoleFaucetEnabled) {
+      router.push('/')
+    }
+  }, [isConsoleFaucetEnabled, router])
 
   return (
-    <div className="flex flex-col w-full items-center py-10 px-6">
-      <div className="text-center mb-10">
-        <Text as="h1" className="text-5xl font-semibold mb-2">
+    <div className="flex flex-col w-full items-center py-10 px-2 sm:px-6 ">
+      <div className="text-center mb-10 px-4">
+        <Text as="h1" className="text-3xl sm:text-5xl font-semibold mb-2">
           Superchain Faucet
         </Text>
-        <Text as="p" className="text-lg text-center text-muted-foreground">
+        <Text
+          as="p"
+          className="text-md sm:text-lg text-center text-muted-foreground"
+        >
           Get test tokens for building applications on the Superchain
         </Text>
       </div>
