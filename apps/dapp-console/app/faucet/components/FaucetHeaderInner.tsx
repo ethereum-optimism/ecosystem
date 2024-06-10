2c89335387543cb7dde73364b7c3034c1eed0a45
Imran Jami <imranajami@gmail.com>
2024-06-10 15:00:44 -0700

    Update FaucetHeaderInner.tsx

--- apps/dapp-console/app/faucet/components/FaucetHeaderInner.tsx
+++ apps/dapp-console/app/faucet/components/FaucetHeaderInner.tsx
@@ -62,7 +62,7 @@ const FaucetHeaderInner = ({ signedIn }: Props) => {
       </>
     )
   } else {
-    // User is signed in and a wallet is connected, but no authenticatio
+    // User is signed in and a wallet is connected, but no authentication
     content = (
       <>
         <Text as="h3" className="text-base font-semibold mb-1">
