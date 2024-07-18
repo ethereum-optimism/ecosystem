a10b2b696b8ef1f0c58c8266d107ea55d718a0fd
Nick Balestra-Foster <nick@balestrafoster.com>
2024-07-17 17:27:05 -0700

    Update vite.config.ts

--- apps/kyc-faq/vite.config.ts
+++ apps/kyc-faq/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: "https://kyc-optimism-io-faq.netlify.app/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
