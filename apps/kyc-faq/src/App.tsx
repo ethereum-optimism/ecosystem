304e00d527bfb74f0ab591fc9442b1df39d9f7bc
jvmi <10838132+jvmi7@users.noreply.github.com>
2024-07-17 18:07:22 -0700

    remove react router (#410)

--- apps/kyc-faq/src/App.tsx
+++ apps/kyc-faq/src/App.tsx
@@ -1,28 +1,15 @@
 import '@/globals.css'
 
-import { RouterProvider, Outlet, createBrowserRouter } from 'react-router-dom'
 import { FaqPage } from '@/components/FaqPage'
 
 const classNames = {
   app: 'app w-full min-h-screen flex flex-col bg-secondary',
 }
 
-const AppRoot = () => {
+export const App = () => {
   return (
     <div className={classNames.app}>
-      <Outlet />
+      <FaqPage />
     </div>
   )
-}
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <AppRoot />,
-    children: [{ path: '/', element: <FaqPage /> }],
-  },
-])
-
-export const App = () => {
-  return <RouterProvider router={router} />
 }
