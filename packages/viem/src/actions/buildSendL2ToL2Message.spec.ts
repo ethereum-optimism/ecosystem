8bf84d1b854c88d098979a981802a9fc10e95da8
italiano@oplabs.co <italiano@oplabs.co>
2024-07-29 11:54:10 -0400

    use tictactoe in build message

--- packages/viem/src/actions/buildSendL2ToL2Message.spec.ts
+++ packages/viem/src/actions/buildSendL2ToL2Message.spec.ts
@@ -1,14 +1,19 @@
-import { toHex } from 'viem'
+import { encodeFunctionData } from 'viem'
 import { base, optimism } from 'viem/chains'
 import { describe, expect, it } from 'vitest'
 
 import { buildSendL2ToL2Message } from '@/actions/buildSendL2ToL2Message.js'
-import { publicClient } from '@/test/clients.js'
+import { publicClient, testAccount } from '@/test/clients.js'
+import { ticTacToeABI, ticTacToeAddress } from '@/test/setupTicTacToe.js'
 
 describe('buildSendL2ToL2Message', () => {
   const expectedChainId = base.id
-  const expectedTarget = '0x057ef64E23666F000b34aE31332854aCBd1c8544'
-  const expectedMessage = toHex(1, { size: 32 })
+  const expectedTarget = ticTacToeAddress
+  const expectedMessage = encodeFunctionData({
+    abi: ticTacToeABI,
+    functionName: 'createGame',
+    args: [testAccount.address],
+  })
 
   it('should return expected request', async () => {
     const res = await buildSendL2ToL2Message(publicClient, {
