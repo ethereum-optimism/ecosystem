ef4efd3a51a6d47c9b8c6e2091981115cd8ec817
italiano@oplabs.co <italiano@oplabs.co>
2024-08-07 14:31:55 -0400

    fix build output

--- packages/viem/tsup.config.ts
+++ packages/viem/tsup.config.ts
@@ -4,10 +4,11 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   name: '@eth-optimism/viem',
   entry: ['src/index.ts'],
-  outDir: 'build',
+  outDir: 'dist',
   format: ['cjs'],
   target: 'node18',
   splitting: false,
   sourcemap: true,
   clean: true,
+  dts: true,
 })
