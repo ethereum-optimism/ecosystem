7f2088a84bf144e4a0da32ebfa0ace33c7618dfa
italiano@oplabs.co <italiano@oplabs.co>
2024-08-07 14:32:20 -0400

    Setup start of @eth-optimism/wagmi package

---
+++ packages/wagmi/src/hooks/useExecuteL2ToL2Message.ts
@@ -0,0 +1,39 @@
+import type { ExecuteL2ToL2MessageParameters } from '@eth-optimism/viem'
+import { contracts, crossL2InboxABI } from '@eth-optimism/viem'
+import { useCallback } from 'react'
+import { useConfig, useWriteContract } from 'wagmi'
+
+export const useExecuteL2ToL2Message = () => {
+  const config = useConfig()
+  const { writeContractAsync } = useWriteContract({ config })
+
+  const sendMessage = useCallback(
+    (params: ExecuteL2ToL2MessageParameters) => {
+      const {
+        id,
+        target,
+        message,
+        nonce,
+        maxFeePerGas,
+        maxPriorityFeePerGas,
+        chain,
+        gas,
+      } = params
+
+      return writeContractAsync({
+        abi: crossL2InboxABI,
+        address: contracts.crossL2Inbox.address,
+        functionName: 'executeMessage',
+        args: [id, target, message],
+        chain,
+        gas: gas ? gas : undefined,
+        nonce,
+        maxFeePerGas,
+        maxPriorityFeePerGas,
+      })
+    },
+    [writeContractAsync],
+  )
+
+  return { sendMessage }
+}
