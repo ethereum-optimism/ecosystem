14ba584bbae1526fb8bd463970fb1c871e59f841
Imran Jami <imranajami@gmail.com>
2024-09-20 11:32:22 -0700

    update config

--- apps/kyc-faq/vite.config.ts
+++ apps/kyc-faq/vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://kyc-optimism-io-faq.netlify.app/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
