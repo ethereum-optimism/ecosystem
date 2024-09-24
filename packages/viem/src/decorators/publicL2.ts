15ca06ccec5f874488576ddba476511bb72da558
italiano@oplabs.co <italiano@oplabs.co>
2024-09-24 10:51:58 -0400

    update exports

--- packages/viem/src/decorators/publicL2.ts
+++ packages/viem/src/decorators/publicL2.ts
@@ -1,72 +1,26 @@
-import type { Account, Address, Chain, Client, Transport } from 'viem'
+import type { Account, Chain, Client, Transport } from 'viem'
 import type { PublicActionsL2 as UpstreamPublicActionsL2 } from 'viem/op-stack'
 import { publicActionsL2 as upstreamPublicActionsL2 } from 'viem/op-stack'
 
-import type {
-  BuildExecuteL2ToL2MessageParameters,
-  BuildExecuteL2ToL2MessageReturnType,
-} from '@/actions/buildExecuteL2ToL2Message.js'
-import { buildExecuteL2ToL2Message } from '@/actions/buildExecuteL2ToL2Message.js'
-import type {
-  BuildSendL2ToL2MessageParameters,
-  BuildSendL2ToL2MessageReturnType,
-} from '@/actions/buildSendL2ToL2Message.js'
-import { buildSendL2ToL2Message } from '@/actions/buildSendL2ToL2Message.js'
-import type {
-  EstimateExecuteL2ToL2MessageGasParameters,
-  EstimateExecuteL2ToL2MessageGasReturnType,
-} from '@/actions/estimateExecuteL2ToL2MessageGas.js'
-import { estimateExecuteL2ToL2MessageGas } from '@/actions/estimateExecuteL2ToL2MessageGas.js'
-import type {
-  EstimateSendL2ToL2MessageGasParameters,
-  EstimateSendL2ToL2MessageGasReturnType,
-} from '@/actions/estimateSendL2ToL2MessageGas.js'
-import { estimateSendL2ToL2MessageGas } from '@/actions/estimateSendL2ToL2MessageGas.js'
+import type { ExecuteL2ToL2MessageParameters } from '@/actions/executeL2ToL2Message.js'
+import { estimateExecuteL2ToL2MessageGas } from '@/actions/executeL2ToL2Message.js'
+import type { SendL2ToL2MessageParameters } from '@/actions/sendL2ToL2Message.js'
+import { estimateSendL2ToL2MessageGas } from '@/actions/sendL2ToL2Message.js'
 
 export type PublicActionsL2<
   chain extends Chain | undefined = Chain | undefined,
   account extends Account | undefined = Account | undefined,
 > = UpstreamPublicActionsL2 & {
-  buildSendL2ToL2Message: <
-    chainOverride extends Chain | undefined = undefined,
-    accountOverride extends Account | Address | undefined = undefined,
-  >(
-    parameters: BuildSendL2ToL2MessageParameters<
-      chain,
-      account,
-      chainOverride,
-      accountOverride
-    >,
-  ) => Promise<BuildSendL2ToL2MessageReturnType>
-  buildExecuteL2ToL2Message: <
-    chainOverride extends Chain | undefined = undefined,
-    accountOverride extends Account | Address | undefined = undefined,
-  >(
-    parameters: BuildExecuteL2ToL2MessageParameters<
-      chain,
-      account,
-      chainOverride,
-      accountOverride
-    >,
-  ) => Promise<BuildExecuteL2ToL2MessageReturnType>
   estimateSendL2ToL2MessageGas: <
     chainOverride extends Chain | undefined = undefined,
   >(
-    parameters: EstimateSendL2ToL2MessageGasParameters<
-      chain,
-      account,
-      chainOverride
-    >,
-  ) => Promise<EstimateSendL2ToL2MessageGasReturnType>
+    parameters: SendL2ToL2MessageParameters<chain, account, chainOverride>,
+  ) => Promise<bigint>
   estimateExecuteL2ToL2MessageGas: <
     chainOverride extends Chain | undefined = undefined,
   >(
-    parameters: EstimateExecuteL2ToL2MessageGasParameters<
-      chain,
-      account,
-      chainOverride
-    >,
-  ) => Promise<EstimateExecuteL2ToL2MessageGasReturnType>
+    parameters: ExecuteL2ToL2MessageParameters<chain, account, chainOverride>,
+  ) => Promise<bigint>
 }
 
 export function publicActionsL2() {
@@ -79,9 +33,6 @@ export function publicActionsL2() {
   ): PublicActionsL2<chain, account> => {
     return {
       ...upstreamPublicActionsL2(),
-      buildSendL2ToL2Message: (args) => buildSendL2ToL2Message(client, args),
-      buildExecuteL2ToL2Message: (args) =>
-        buildExecuteL2ToL2Message(client, args),
       estimateSendL2ToL2MessageGas: (args) =>
         estimateSendL2ToL2MessageGas(client, args),
       estimateExecuteL2ToL2MessageGas: (args) =>
--- packages/viem/src/index.ts
+++ packages/viem/src/index.ts
@@ -10,7 +10,7 @@ export { supersimL1, supersimL2A, supersimL2B } from '@/chains/supersim.js'
 // contracts
 export { contracts } from '@/contracts.js'
 
-// public actions L2
+// actions
 export type {
   BuildExecuteL2ToL2MessageParameters,
   BuildExecuteL2ToL2MessageReturnType,
@@ -21,32 +21,28 @@ export type {
   BuildSendL2ToL2MessageReturnType,
 } from '@/actions/buildSendL2ToL2Message.js'
 export { buildSendL2ToL2Message } from '@/actions/buildSendL2ToL2Message.js'
-export type {
-  EstimateExecuteL2ToL2MessageGasErrorType,
-  EstimateExecuteL2ToL2MessageGasParameters,
-  EstimateExecuteL2ToL2MessageGasReturnType,
-} from '@/actions/estimateExecuteL2ToL2MessageGas.js'
-export { estimateExecuteL2ToL2MessageGas } from '@/actions/estimateExecuteL2ToL2MessageGas.js'
-export type {
-  EstimateSendL2ToL2MessageGasErrorType,
-  EstimateSendL2ToL2MessageGasParameters,
-  EstimateSendL2ToL2MessageGasReturnType,
-} from '@/actions/estimateSendL2ToL2MessageGas.js'
-export { estimateSendL2ToL2MessageGas } from '@/actions/estimateSendL2ToL2MessageGas.js'
 
-// wallet actions L2
+// actions
 export type {
   ExecuteL2ToL2MessageErrorType,
   ExecuteL2ToL2MessageParameters,
   ExecuteL2ToL2MessageReturnType,
 } from '@/actions/executeL2ToL2Message.js'
-export { executeL2ToL2Message } from '@/actions/executeL2ToL2Message.js'
+export {
+  estimateExecuteL2ToL2MessageGas,
+  executeL2ToL2Message,
+  simulateExecuteL2ToL2Message,
+} from '@/actions/executeL2ToL2Message.js'
 export type {
   SendL2ToL2MessageErrorType,
   SendL2ToL2MessageParameters,
   SendL2ToL2MessageReturnType,
 } from '@/actions/sendL2ToL2Message.js'
-export { sendL2ToL2Message } from '@/actions/sendL2ToL2Message.js'
+export {
+  estimateSendL2ToL2MessageGas,
+  sendL2ToL2Message,
+  simulateSendL2ToL2Message,
+} from '@/actions/sendL2ToL2Message.js'
 
 // utils
 export type {
