87e067562d9fa0f145e029b154b5878a408b40e3
italiano@oplabs.co <italiano@oplabs.co>
2024-09-24 10:51:46 -0400

    update tests

--- packages/viem/src/actions/sendL2ToL2Message.spec.ts
+++ packages/viem/src/actions/sendL2ToL2Message.spec.ts
@@ -11,53 +11,74 @@ import { decodeSentMessage } from '@/utils/decodeSentMessage.js'
 import { extractMessageIdentifierFromLogs } from '@/utils/extractMessageIdentifierFromLogs.js'
 
 describe('sendL2ToL2Message', () => {
-  it('should return expected request', async () => {
-    const encodedData = encodeFunctionData({
-      abi: ticTacToeABI,
-      functionName: 'createGame',
-      args: [testAccount.address],
+  describe('write contract', () => {
+    it('should return expected request', async () => {
+      const encodedData = encodeFunctionData({
+        abi: ticTacToeABI,
+        functionName: 'createGame',
+        args: [testAccount.address],
+      })
+
+      const args = await buildSendL2ToL2Message(publicClient, {
+        account: testAccount.address,
+        destinationChainId: supersimL2B.id,
+        target: ticTacToeAddress,
+        message: encodedData,
+      })
+
+      const hash = await walletClient.sendL2ToL2Message(args)
+      expect(hash).toBeDefined()
+
+      const receipt = await publicClient.waitForTransactionReceipt({ hash })
+      const { id, payload } = await extractMessageIdentifierFromLogs(
+        publicClient,
+        { receipt },
+      )
+
+      // verifiy message id
+      expect(id).toBeDefined()
+      expect(id.chainId).toEqual(BigInt(supersimL2A.id))
+      expect(id.origin.toLowerCase()).toEqual(
+        contracts.l2ToL2CrossDomainMessenger.address.toLowerCase(),
+      )
+      expect(id.blockNumber).toEqual(receipt.blockNumber)
+      expect(id.logIndex).toEqual(BigInt(receipt.logs[0].logIndex))
+
+      // verify payload
+      const currentNonce = await publicClient.readContract({
+        abi: l2ToL2CrossDomainMessengerABI,
+        address: contracts.l2ToL2CrossDomainMessenger.address,
+        functionName: 'messageNonce',
+      })
+
+      const decodedPayload = decodeSentMessage({ payload })
+      expect(decodedPayload).toEqual({
+        origin: BigInt(supersimL2A.id),
+        destination: BigInt(supersimL2B.id),
+        messageNonce: currentNonce - 1n,
+        sender: testAccount.address,
+        target: ticTacToeAddress,
+        message: encodedData,
+      })
     })
+  })
 
-    const args = await buildSendL2ToL2Message(publicClient, {
-      account: testAccount.address,
-      destinationChainId: supersimL2B.id,
-      target: ticTacToeAddress,
-      message: encodedData,
-    })
+  describe('estimate gas', () => {
+    it('should estimate gas', async () => {
+      const encodedMessage = encodeFunctionData({
+        abi: ticTacToeABI,
+        functionName: 'createGame',
+        args: [testAccount.address],
+      })
 
-    const hash = await walletClient.sendL2ToL2Message(args)
-    expect(hash).toBeDefined()
+      const gas = await publicClient.estimateSendL2ToL2MessageGas({
+        account: testAccount.address,
+        target: ticTacToeAddress,
+        destinationChainId: supersimL2B.id,
+        message: encodedMessage,
+      })
 
-    const receipt = await publicClient.waitForTransactionReceipt({ hash })
-    const { id, payload } = await extractMessageIdentifierFromLogs(
-      publicClient,
-      { receipt },
-    )
-
-    // verifiy message id
-    expect(id).toBeDefined()
-    expect(id.chainId).toEqual(BigInt(supersimL2A.id))
-    expect(id.origin.toLowerCase()).toEqual(
-      contracts.l2ToL2CrossDomainMessenger.address.toLowerCase(),
-    )
-    expect(id.blockNumber).toEqual(receipt.blockNumber)
-    expect(id.logIndex).toEqual(BigInt(receipt.logs[0].logIndex))
-
-    // verify payload
-    const currentNonce = await publicClient.readContract({
-      abi: l2ToL2CrossDomainMessengerABI,
-      address: contracts.l2ToL2CrossDomainMessenger.address,
-      functionName: 'messageNonce',
-    })
-
-    const decodedPayload = decodeSentMessage({ payload })
-    expect(decodedPayload).toEqual({
-      origin: BigInt(supersimL2A.id),
-      destination: BigInt(supersimL2B.id),
-      messageNonce: currentNonce - 1n,
-      sender: testAccount.address,
-      target: ticTacToeAddress,
-      message: encodedData,
+      expect(gas).toBeDefined()
     })
   })
 })
