55f4a9a7c88b674bbea701b833cca1fbacf2cc60
italiano@oplabs.co <italiano@oplabs.co>
2024-10-22 14:08:05 -0400

    Wait for supersim to be ready

--- packages/viem/src/test/startSupersim.ts
+++ packages/viem/src/test/startSupersim.ts
@@ -1,3 +1,5 @@
+import fs from 'fs'
+import path from 'path'
 import { spawn } from 'child_process'
 
 export type SupersimParameters = {
@@ -6,6 +8,10 @@ export type SupersimParameters = {
 }
 
 export type SupersimReturnArg = Promise<() => void>
+
+const READY_TEXT = 'supersim is ready'
+const STD_OUT_PATH = path.resolve(__dirname, './stdout.txt')
+const STD_ERR_PATH = path.resolve(__dirname, './stderr.txt')
 
 export async function startSupersim(
   params?: SupersimParameters,
@@ -20,11 +26,32 @@ export async function startSupersim(
     args.push(` --l2.starting.port ${params.l2StartingPort}`)
   }
 
+  fs.writeFileSync(STD_OUT_PATH, '')
+  fs.writeFileSync(STD_ERR_PATH, '')
+
   const supersimProcess = spawn('supersim', args, { shell: true })
 
-  return () => {
+  const cleanup = () => {
     if (!supersimProcess.killed) {
       supersimProcess.kill()
     }
   }
+
+  return new Promise((resolve, reject) => {
+    supersimProcess.stdout.on('data', (data) => {
+      const blob: string = data.toString()
+
+      fs.appendFileSync(STD_OUT_PATH, blob.replace(/\\n/g, '\n'))
+
+      if (blob.includes(READY_TEXT)) {
+        resolve(cleanup)
+      }
+    })
+
+    supersimProcess.stderr.on('data', (data) => {
+      const blob: string = data.toString()
+      fs.appendFileSync(STD_ERR_PATH, blob.replace(/\\n/g, '\n'))
+      reject(blob)
+    })
+  })
 }
