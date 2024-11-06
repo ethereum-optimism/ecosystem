1698047be871d26970c3fa026fc05ac8b9f2c7ce
tre <tremarkley@gmail.com>
2024-11-06 11:41:34 -0800

    feat(supersim): bump to version 0.1.0-alpha.26 (#568)
    feat(supersim): bump to version 0.1.0-alpha.25

--- packages/supersim/install.js
+++ packages/supersim/install.js
@@ -7,7 +7,7 @@ const { exec } = require('child_process')
 const PLATFORM = os.platform()
 const ARCH = os.arch()
 const BIN_PATH = path.join(__dirname, 'bin')
-const SUPERSIM_VERSION = '0.1.0-alpha.24'
+const SUPERSIM_VERSION = '0.1.0-alpha.26'
 
 const archiveFilename = {
   darwin: {
