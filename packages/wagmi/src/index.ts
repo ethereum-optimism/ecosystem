c45280e1e97028c0140b389e3b12fb5905683203
italiano@oplabs.co <italiano@oplabs.co>
2024-12-12 17:06:31 -0500

    lint

--- packages/wagmi/src/index.ts
+++ packages/wagmi/src/index.ts
@@ -1,6 +1,6 @@
 // hooks
+export { useCrossChainSendETH } from '@/hooks/useCrossChainSendETH.js'
 export { useRelayL2ToL2Message } from '@/hooks/useRelayL2ToL2Message.js'
 export { useSendL2ToL2Message } from '@/hooks/useSendL2ToL2Message.js'
-export { useCrossChainSendETH } from '@/hooks/useCrossChainSendETH.js'
 export { useSendSupERC20 } from '@/hooks/useSendSupERC20.js'
 export { useSendSuperchainWETH } from '@/hooks/useSendSuperchainWETH.js'
