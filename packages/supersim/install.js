195ce15ac1286d6c07fbb1e7b83f6c65623c6fbf
tre <tremarkley@gmail.com>
2025-02-14 12:14:50 -0800

    feat(supersim): 0.1.0-alpha.37 release (#658)

--- packages/supersim/install.js
+++ packages/supersim/install.js
@@ -7,7 +7,7 @@ const { exec } = require('child_process')
 const PLATFORM = os.platform()
 const ARCH = os.arch()
 const BIN_PATH = path.join(__dirname, 'bin')
-const SUPERSIM_VERSION = '0.1.0-alpha.36'
+const SUPERSIM_VERSION = '0.1.0-alpha.37'
 
 const archiveFilename = {
   darwin: {
