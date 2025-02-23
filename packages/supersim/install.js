410eba43d9c80e6fc189e5f1fb0a0f1055dbd007
James Kim <jakim929@gmail.com>
2025-02-22 19:20:28 -0800

    chore: bump supersim

--- packages/supersim/install.js
+++ packages/supersim/install.js
@@ -7,7 +7,7 @@ const { exec } = require('child_process')
 const PLATFORM = os.platform()
 const ARCH = os.arch()
 const BIN_PATH = path.join(__dirname, 'bin')
-const SUPERSIM_VERSION = '0.1.0-alpha.41'
+const SUPERSIM_VERSION = '0.1.0-alpha.42'
 
 const archiveFilename = {
   darwin: {
