12e1faf11a7ce6bbf748e936ef2e674c4b418dda
Hamdi Allam <hamdi.allam97@gmail.com>
2025-04-05 21:53:02 +0200

    most of the way there :fire:

--- apps/mock-sponsored-sender/src/api.ts
+++ apps/mock-sponsored-sender/src/api.ts
@@ -1,37 +1,67 @@
-import { Hono } from "hono";
+import { Hono } from 'hono'
+import { z } from 'zod'
 
-const api = new Hono();
+import type { JsonRpcHandler } from '@/jsonrpc/handler.js'
+import type { JsonRpcResponse } from '@/jsonrpc/types.js'
+import { parseRequestBody } from '@/jsonrpc/utils.js'
 
-// Global Middleware
+const sponsoredEndpointSchema = z.object({
+  chainId: z.coerce.number().int().positive(),
+})
 
-api.use(async (c, next) => {
+/**
+ * Create a Hono API Router that routes JSON RPC requests to the correct
+ * handler based on the chain ID.
+ * @param handlers - A map of chain IDs to JSON RPC handlers.
+ * @returns A Hono API Router.
+ */
+export function createApiRouter(handlers: Record<number, JsonRpcHandler>) {
+  const api = new Hono()
+
+  // global middleware
+  api.use(async (c, next) => {
     const { req } = c
     const now = Date.now()
     await next()
     const ms = Date.now() - now
-    console.log(`[${new Date(now).toISOString()}] ${req.method} ${req.url} - ${ms}ms`)
-});
+    console.log(
+      `[${new Date(now).toISOString()}] ${req.method} ${req.url} - ${ms}ms`,
+    )
+  })
 
-// Sponsored Endpoint
-
-api.post("/:chainId", async (c) => {
-    const { chainId } = c.req.param()
-    if (!chainId) {
-        return c.json({ error: "chainId is required" }, 400)
+  // route json rpc requests based on chainId
+  api.post('/:chainId', async (c) => {
+    if (c.req.header('Content-Type') !== 'application/json') {
+      return c.json({ error: 'Content-Type must be application/json' }, 400)
     }
 
-    // todo validate the chain id
-
-    if (c.req.header("Content-Type") !== 'application/json') {
-        return c.json({ error: "Content-Type must be application/json" }, 400)
+    const params = c.req.param()
+    const result = sponsoredEndpointSchema.safeParse(params)
+    if (!result.success) {
+      return c.json({ error: result.error.format() }, 400)
     }
 
-    // grab the router for this chain and handle
+    const chainId = result.data.chainId
+    const handler = chainId in handlers ? handlers[chainId] : null
+    if (!handler) {
+      return c.json({ error: 'unregistered chainId' }, 400)
+    }
 
     const body = await c.req.json()
-    console.log(body)
+    const bodyResult = parseRequestBody(body)
+    if (bodyResult.error) {
+      const resp = {
+        jsonrpc: '2.0',
+        id: null,
+        error: { code: -32600, message: 'Invalid request' },
+      } satisfies JsonRpcResponse
 
-    return c.text("ok")
-});
+      return c.json(resp)
+    }
 
-export default api
+    const response = await handler.handle(bodyResult.data)
+    return c.json(response)
+  })
+
+  return api
+}
