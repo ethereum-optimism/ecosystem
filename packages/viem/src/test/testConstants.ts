222c93d6f6b2653f4120b5cae4e3ba55502b8c48
tre <tremarkley@gmail.com>
2025-04-14 11:53:12 -0700

    release(supersim): release 0.1.0-alpha.47

--- packages/viem/src/test/testConstants.ts
+++ packages/viem/src/test/testConstants.ts
@@ -1,7 +1,7 @@
 // Hardcoded since we don't have a good way to pull the L1 contracts for supersim yet.
 export const supersimL2AL1StandardBridgeAddress =
-  '0xe5779130C1b1265Cfb811fCd6c7D47Ca4fE890dd'
+  '0xFD840Ec94235a8884881a68BB93d0110C9955604'
 
 // Hardcoded since we don't have a good way to pull the L1 contracts for supersim yet.
 export const supersimL2AL1CrossDomainMessengerAddress =
-  '0x67b53cC5074ddB89A50358C4eabF5EED6A5AE135'
+  '0x1D9234B7E5e720ef9022A64DB5e39E99708dC229'
