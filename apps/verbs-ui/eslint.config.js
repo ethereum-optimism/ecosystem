23647c68b827ac6d9b6afaa6a16df93ab2ba1bd9
its-everdred <kevinweaver2@gmail.com>
2025-07-16 13:47:10 -0700

    pnpm lint:fix

--- apps/verbs-ui/eslint.config.js
+++ apps/verbs-ui/eslint.config.js
@@ -3,17 +3,14 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  { ignores: ['dist'] },
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      ...tseslint.configs.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
