1ec388fbd00c1f21ae2fcc46ac121b55a2be1fca
its-everdred <kevinweaver2@gmail.com>
2025-07-16 12:48:57 -0700

    pnpm create vite verbs-ui --template react-ts

--- apps/verbs-ui/eslint.config.js
+++ apps/verbs-ui/eslint.config.js
@@ -3,14 +3,17 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
-  { ignores: ['dist'] },
+  globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
