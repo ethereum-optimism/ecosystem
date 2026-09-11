d040e69b933710e0006742f1246c1dd78ba35826
Harry Markley <harry@oplabs.co>
2026-09-11 11:51:15 -0700

    update l1 addresses

--- packages/viem/src/test/testConstants.ts
+++ packages/viem/src/test/testConstants.ts
@@ -1,7 +1,7 @@
 // Hardcoded since we don't have a good way to pull the L1 contracts for supersim yet.
 export const supersimL2AL1StandardBridgeAddress =
-  '0x52AF4Dff31d13449b70993E21eb599C615a66541'
+  '0x80436d125650b9f2f1e628667d7de9e46d16a1ef'
 
 // Hardcoded since we don't have a good way to pull the L1 contracts for supersim yet.
 export const supersimL2AL1CrossDomainMessengerAddress =
-  '0xc8c71093ffc2092D61A72369d81940B839Bb12b4'
+  '0x50f27efea9a62b3c5748d78789e9fc558c50edda'
